32c2ad548f82f6b5a1a726a48894e4a944b52314
Kamryn Smith <programersign@gmail.com>
2023-02-01 11:30:15 -0800

    assignment complete

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(someValue1) {
+    if (someValue1 >= 42) {
+        return someValue1 - 42
+    } else if (someValue1 <= 42) {
+        return 42 - someValue1
+    }
+}
+
+function distanceFromHqInFeet(someValue2) {
+    return distanceFromHqInBlocks(someValue2) * 264
+}
+
+function distanceTravelledInFeet(start, end) {
+    if (start >= end) {
+        return (start - end)*264
+    } else if (start <= end) {
+        return (end - start)*264
+    }
+}
+
+function calculatesFarePrice(start, destination) {
+    let total = 0;
+    if (start <= destination){
+        total = (destination - start)*264
+    } else if (start >= destination) {
+        total = (start - destination)*264
+    }
+    if (total <= 400){
+        return 0
+    } else if (total > 400 && total <= 2000) {
+        return (total - 400)* 0.02
+    } else if (total > 2000 && total < 2500) {
+        return 25
+    } else if (total > 2500) {
+        return "cannot travel that far"
+    }
+}
